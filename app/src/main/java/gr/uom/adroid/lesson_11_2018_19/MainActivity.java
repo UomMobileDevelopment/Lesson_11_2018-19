f877bf3291dc538e2a0c3d3ea9465e219c5c2517
teohaik <teohaik@gmail.com>
2018-12-17 21:25:00 +0200

    added onItemClickListener

--- app/src/main/java/gr/uom/adroid/lesson_11_2018_19/MainActivity.java
+++ app/src/main/java/gr/uom/adroid/lesson_11_2018_19/MainActivity.java
@@ -1,7 +1,11 @@
 package gr.uom.adroid.lesson_11_2018_19;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,6 +25,23 @@ public class MainActivity extends AppCompatActivity {
 
         newsAdapter = new NewsAdapter(this, R.layout.news_list_item, new ArrayList<NewsEntry>());
         listView.setAdapter(newsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+
+                NewsEntry newsEntry = newsAdapter.getNewsEntry(position);
+                String url = newsEntry.getUrl();
+
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+        });
+
+
+
         fetchNews();
     }
 
